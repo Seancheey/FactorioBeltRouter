fb4f5764bec6d8f0e03d8b2170944f1743877055
Qiyi Shan <adls371@outlook.com>
2020-10-08 02:42:36 -0700

    adds player settings for path finding speed

--- transport_line_connector.lua
+++ transport_line_connector.lua
@@ -243,7 +243,7 @@ function TransportLineConnector:buildTransportLine(startingEntity, endingEntity,
     -- A* algorithm starts from endingEntity so that we don't have to consider/change last belt's direction
     priorityQueue:push(0, TransportChain.new(endingEntity))
     local maxTryNum = 20000
-    local batchSize = 10
+    local batchSize = settings.get_player_settings(player)["path-finding-test-per-tick"].value
     local totalTryNum = 0
     local taskPriority = game.tick
     local function tryFindPath()
