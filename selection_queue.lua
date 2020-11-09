d98363628f50a1ce3804538f09483f27c020dd40
Qiyi Shan <adls371@outlook.com>
2020-11-08 23:24:33 -0800

    Update label to other selection

--- selection_queue.lua
+++ selection_queue.lua
@@ -145,6 +145,7 @@ function SelectionQueue:__updateLabelNumbers()
             rendering.draw_text {
                 surface = game.players[self.playerIndex].surface,
                 players = { game.players[self.playerIndex] },
+                target = otherSelection.ghostPosition,
                 text = i,
                 color = { 1, 1, 1, 0.9 },
                 time_to_live = renderedBoxLiveTime
